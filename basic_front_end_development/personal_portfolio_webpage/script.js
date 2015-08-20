ea9b7cf10c0c329deb10d1afbfa63965c497cc07
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-08-20 21:27:43 +0200

    Updated script.js

--- basic_front_end_development/personal_portfolio_webpage/script.js
+++ basic_front_end_development/personal_portfolio_webpage/script.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+  if($(window).width() <= 800){
+    $(h1).css("font-size", "37px");
+  }
+}
+      
+$(window).resize(function() {
+  if($(window).width() <= 800){
+    $(h1).css("font-size", "37px");
+  }
+})

2d1ce35d3aa2a532efe0fa42eba6f4747ccbbfd6
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-08-27 12:12:20 +0200

    Changes to structure
    Removed /js folder - won't be storing the quotes in a separate file any
    more. Started adding quotes to script.js.

--- basic_front_end_development/random_quote_machine/script.js
+++ basic_front_end_development/random_quote_machine/script.js
@@ -1,1 +1,12 @@
+var engQuotes = [
+["Humans need fantasy to be human. To be the place where the falling angel meets the rising ape.", "Terry Pratchett, Hogfather"],
+["Yeah, science!", "Jesse Pinkman"],
+["Run. If you can't run, walk. If you can't walk, crawl. But by all means, keep on moving.", "Martin Luther King"],
+["Test", "Reneald"]
+["What’s the difference between a hippo and a Zippo? One is really heavy, the other is a little lighter.", "Masai Graham"],
+]
 
+var dutchQuotes ] [
+["Wie van zichzelf houdt geeft pas echt iets kostbaars als hij Ik hou van jou tegen een ander zegt", "Harry Jekkers"],
+["Geef mij een lach, geef mij wat zonneschijn", "Chirojeugd Vlaanderen"]
+]

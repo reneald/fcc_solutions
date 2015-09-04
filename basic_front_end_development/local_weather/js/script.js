4ece8d0a2d190508d897e6237e450b958eceebdf
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-09-04 15:56:14 +0200

    updates to html and js

--- basic_front_end_development/local_weather/js/script.js
+++ basic_front_end_development/local_weather/js/script.js
@@ -1,19 +1,22 @@
+"use strict";
 $(document).ready(function() {
+	/* API key doesn't seem necessary?
 	var apiKey = "32a0785e5cc9ee1264cd122fd739d9b9";
-	// set var 'unit' so we can change between Celsius and Fahrenheit
+	*/
+	// set var "unit" so we can change between Celsius and Fahrenheit
 	var unit = ["metric", "C"];
 
-	// get user's location
-	
-	var lat = $.getJSON('http://www.telize.com/geoip/185.61.75.15',
+	// get user"s location
+
+	var lat = $.getJSON("http://www.telize.com/geoip/185.61.75.15",
 		function(loc) {
-			return loc['latitude'];
+			return loc.latitude;
 		});
-	var lon = $.getJSON('http://www.telize.com/geoip',
+	var lon = $.getJSON("http://www.telize.com/geoip",
 		function(loc) {
-			return loc['longitude'];
+			return loc.longitude;
 		});
-	
+
 	$("#weather").prepend(lat);
 
 
@@ -30,19 +33,18 @@ $(document).ready(function() {
 			unit[0] = "imperial";
 			unit[1] = "F";
 			$("#unit").text("Celsius");
-		}
-		else {
+		} else {
 			unit[0] = "metric";
 			unit[1] = "C";
 			$("#unit").text("Fahrenheit");
-		};
+		}
 		getWeather(lat, lon, unit);
 	});
 
 	// send request to OpenWeatherMap
 	/* Function for forecast. Not in use right now, we'll get the current weather to work now and maybe change to forecast later.
 
-	$.getJSON('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + "&units=" + unit + '&callback=?', 
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=" + unit + "&callback=?", 
 		function(result) {
 			asldkjfa;lskjf
 		}
@@ -51,11 +53,11 @@ $(document).ready(function() {
 
 	/* Function for current weather */
 	function getWeather(lat, lon, unit) {
-		$.getJSON('//api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + "&units=" + unit[0] + '&callback=?', 
+		$.getJSON("//api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + unit[0] + "&callback=?",
 			function(result) {
 				$("#weather").text("It is " + result.main.temp + " \xB0" + unit[1] + " in " + result.name);
 			}
 		);
-	};
-	
+	}
+
 });

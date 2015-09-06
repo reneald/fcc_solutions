931d21addc0915c5bcf128c2fabc0e3179baba18
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-09-06 16:01:31 +0200

    .

--- basic_front_end_development/local_weather/js/script.js
+++ basic_front_end_development/local_weather/js/script.js
@@ -8,16 +8,16 @@ $(document).ready(function() {
 	var lon = 139;
 	var temp = 20.0;
 
-/* going to test geolocation API
-	//get user's location
-	$.getJSON("http://www.telize.com/geoip",
-		function(loc) {
-			lat = loc.latitude.toString();
-			lon = loc.longitude.toString();
-			getWeather(lat, lon, unit);
-		}
-	);
-*/
+	/* going to test geolocation API
+		//get user's location
+		$.getJSON("http://www.telize.com/geoip",
+			function(loc) {
+				lat = loc.latitude.toString();
+				lon = loc.longitude.toString();
+				getWeather(lat, lon, unit);
+			}
+		);
+	*/
 	getLocation();
 
 
@@ -51,9 +51,9 @@ $(document).ready(function() {
 	function getLocation() {
 		if (Modernizr.geolocation) {
 			navigator.geolocation.getCurrentPosition(showWeather, geoLocError);
-		}
-		else {
-			$("#it-is").text("Your browser doesn't support geolocation. You won't be able to use this site until I update it with some other functionality");
+		} else {
+			$("#debug").text("Your browser doesn't support geolocation. You won't be able to use this site until I update it with some other functionality");
+			$("#it-is").empty();
 			$("#weather").empty();
 			$("#location").empty();
 			$("#pref").empty();
@@ -63,7 +63,7 @@ $(document).ready(function() {
 	function showWeather(position) {
 		lat = position.coords.latitude;
 		lon = position.coords.longitude;
-		$("#debug").append("latitude: " + lat + " longitude: " + lon);
+		//$("#debug").append("latitude: " + lat + " longitude: " + lon);
 		getWeather(lat, lon, unit);
 	}
 
@@ -74,15 +74,13 @@ $(document).ready(function() {
 			$("#weather").empty();
 			$("#location").empty();
 			$("#pref").empty();
-		}
-		else if(err.code == 2) {
+		} else if (err.code == 2) {
 			$("#debug").text("I'm sorry, I couldn't get your location because of some obscure problem involving satellites. Maybe one broke down and crashed in your backyard. Could you check that and then reload this site?");
 			$("#it-is").empty();
 			$("#weather").empty();
 			$("#location").empty();
 			$("#pref").empty();
-		}
-		else if(err.code == 3) {
+		} else if (err.code == 3) {
 			$("#debug").text("I'm trying to get your location but this is taking too long and I'm getting bored. Please try again later.");
 			$("#it-is").empty();
 			$("#weather").empty();
@@ -97,6 +95,8 @@ $(document).ready(function() {
 			function(result) {
 				temp = result.main.temp;
 				$("#weather").text(temp + "\xB0" + unit[1]);
+				$("#weather-icon").prepend('<img src="http://openweathermap.org/img/w/' + result.weather[0].icon + '.png" />');
+				$("#description").text(titleCase(result.weather[0].description));
 				$("#location").append(result.name);
 			}
 		);
@@ -112,5 +112,13 @@ $(document).ready(function() {
 		temp = (temp - 32) * 5 / 9;
 		temp = Math.round(temp * 100) / 100;
 	}
+
+
+	// Function for formatting the weather description
+	function titleCase(str) {
+		return str.toLowerCase().split(' ').map(function(val) {
+		return (val.charAt(0).toUpperCase() + val.substr(1));
+	}).join(' ');
+}
 
 });

2e40d8a2e6fbc33fbca67df555e81634ca8c573d
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-09-10 15:18:31 +0200

    .

--- basic_front_end_development/pomodoro_clock/script.js
+++ basic_front_end_development/pomodoro_clock/script.js
@@ -1,11 +1,52 @@
 $(document).ready(function() {
-  var timer = 1000;
-  setInterval(countdown(), 1000)
+	var origTimer = 1500;
+	var timer = 1500;
+	var seconds = timer % 60;
+	var minutes = Math.floor(timer / 60);
+	var playPause = $("#start-button").text();
+	var pomodoro;
 
-  function countdown() {
-  	while (timer > 0) {
-  		$("#countdown").text(timer);
-  		timer-=
-  	}
-  }
+	$("#countdown").text(minutes + ":0" + seconds);
+	$("#start-button").click(function() {
+		if (playPause == "play_arrow") {
+			$("#start-button").text("pause");
+			playPause = $("#start-button").text();
+			pomodoro = setInterval(countdown, 1000);
+		} else {
+			$("#start-button").text("play_arrow");
+			playPause = $("#start-button").text();
+			clearInterval(pomodoro);
+		}
+
+	});
+
+	$("#replay-button").click(replay);
+
+
+	function countdown() {
+		if (timer > 0) {
+			if (seconds < 10) {
+				$("#countdown").text(minutes + ":0" + seconds);
+			} else {
+				$("#countdown").text(minutes + ":" + seconds);
+			}
+			timer--;
+			seconds = timer % 60;
+			minutes = Math.floor(timer / 60);
+		} else {
+			$("#countdown").text("Boom");
+		}
+	}
+
+	function replay() {
+		clearInterval(pomodoro);
+		timer = origTimer;
+		var seconds = timer % 60;
+		var minutes = Math.floor(timer / 60);
+		$("#countdown").text(minutes + ":0" + seconds);
+		$("#countdown").text(minutes + ":0" + seconds);
+		if (playPause == "pause") {
+			$("#start-button").text("play_arrow");
+		}
+	}
 });

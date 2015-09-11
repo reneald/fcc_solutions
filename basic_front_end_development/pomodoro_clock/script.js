1b5148147fda8035f13353aa135f566fc6c059e1
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-09-11 20:09:53 +0200

    make timer accurate
    countdown still keeps going in background after pause button is clicked.

--- basic_front_end_development/pomodoro_clock/script.js
+++ basic_front_end_development/pomodoro_clock/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	var origTimer = 1500;
 	var timer = 1500;
-	var seconds = timer % 60;
+  var startTime = new Date().getTime();
+	var seconds = Math.floor(timer % 60);
 	var minutes = Math.floor(timer / 60);
 	var playPause = $("#start-button").text();
 	var pomodoro;
@@ -11,17 +12,16 @@ $(document).ready(function() {
 		if (playPause == "play_arrow") {
 			$("#start-button").text("pause");
 			playPause = $("#start-button").text();
-			pomodoro = setInterval(countdown, 1000);
+			pomodoro = setInterval(countdown, 100);
 		} else {
 			$("#start-button").text("play_arrow");
-			playPause = $("#start-button").text();
+			playPause = $("#start-button").text();			
 			clearInterval(pomodoro);
 		}
 
 	});
 
 	$("#replay-button").click(replay);
-
 
 	function countdown() {
 		if (timer > 0) {
@@ -30,23 +30,23 @@ $(document).ready(function() {
 			} else {
 				$("#countdown").text(minutes + ":" + seconds);
 			}
-			timer--;
-			seconds = timer % 60;
+			var time = new Date().getTime() - startTime;
+      timer = origTimer - (Math.floor(time/100)/10);
+			seconds = Math.floor(timer % 60);
 			minutes = Math.floor(timer / 60);
 		} else {
 			$("#countdown").text("Boom");
 		}
 	}
-
-	function replay() {
-		clearInterval(pomodoro);
+  
+  function replay() {
+    clearInterval(pomodoro);
 		timer = origTimer;
 		var seconds = timer % 60;
 		var minutes = Math.floor(timer / 60);
 		$("#countdown").text(minutes + ":0" + seconds);
-		$("#countdown").text(minutes + ":0" + seconds);
-		if (playPause == "pause") {
-			$("#start-button").text("play_arrow");
-		}
-	}
+    if (playPause == "pause") {
+      $("#start-button").text("play_arrow");
+    }
+  }
 });

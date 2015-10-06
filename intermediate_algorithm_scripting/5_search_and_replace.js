798fad281ecac380d2f5c9db83bc46ff579b4a1c
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-10-06 16:59:52 +0200

    Added two more solutions

---
+++ intermediate_algorithm_scripting/5_search_and_replace.js
@@ -0,0 +1,14 @@
+function replace(str, before, after) {
+  str.split(' ').map(function(curr) {
+    if (curr == before) {
+      curr.split('').map(function(currChar) {
+        if (currChar == currChar.toUpperCase()) {
+          after = after.charAt(0).toUpperCase() + after.substr(1);
+        }
+      }).join('');
+    }
+  }).join(' ');
+ return str.replace(before, after);
+}
+
+replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");

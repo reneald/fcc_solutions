380dff50f164857c41c2ad8ae87cf8d79a8556ee
Leander Van Reeth <delea.vanreeth@gmail.com>
2015-12-22 15:30:05 +0000

    added some styling to javascript calculator

---
+++ basic_front_end_development/javascript_calculator/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    "use strict";
+    var input = [];
+    var current = "";
+    $("button").click(function() {
+        // current = current.concat(this.html());
+        $("footer").html("success");
+    })
+    
+})

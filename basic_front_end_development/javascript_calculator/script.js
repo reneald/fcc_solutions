ee7d5786ed1fe1bdb6f71d0cb3a767cc881e3cff
Leander Van Reeth <delea.vanreeth@gmail.com>
2016-01-05 16:00:54 +0000

    Made AC, CE work, initialized work on operators

--- basic_front_end_development/javascript_calculator/script.js
+++ basic_front_end_development/javascript_calculator/script.js
@@ -1,19 +1,37 @@
 $(document).ready(function() {
-    "use strict";
-    let inputs = [];
-    let total = 0;
-    let current = "";
-    $("button").click(function() {
-        // current = current.concat(this.html());
-        let val = $(this).text();
+  "use strict";
+  let total = 0;
+  let current = "";
+    
+  $("button").click(function() {
+      // current = current.concat(this.html());
+      let val = $(this).text();
         
-        //clicked a number? --> store in 'current'
-        if (!isNaN(val) || val === ".") {
-            current += val;
-            $("#answer").val(current.substr(0, 10));
-        }
+      // Clicked a number? --> store in 'current'
+      if (!isNaN(val) || val === ".") {
+        current += val;
+        $("#answer").val(current.substr(0, 10));
+      } else if (val === "CE") {
+        // Only current number must be deleted, total and operator should remain.
+        current = "";
+      } else if (val === "AC") {
+        // Reset calculator entirely
+        current = "";
+        total = "";
+      }
         
-        //using next line to test function
-    })
+        
+  })
+  
+  /* Function for operators
+  Should do the following:
+   - store the operator in optor
+   - 
+  */
+  
+  function operate(operator) {
+
+  }
     
 })
+

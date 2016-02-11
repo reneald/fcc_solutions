1b4f80a2c92fc03e5026e6cbecccbb641171a702
Leander Van Reeth <delea.vanreeth@gmail.com>
2016-02-11 12:36:07 +0000

    Fixed calculator js: basic operators work
    - Fixed CALCULATE function. Unfortunately, repeatedly clicking the same operator button now doesn't work.
    - Added AC and CE functionality to OPERATORS array, but they don't work yet. Cause unidentified.

--- basic_front_end_development/javascript_calculator/test_script.js
+++ basic_front_end_development/javascript_calculator/test_script.js
@@ -3,14 +3,15 @@ $(document).ready(function() {
   let total = 0;
   let current = "0";
   const operators = {
-    "+": function(a, b) {return a + b},
-    "-": function(a, b) {return a - b},
-    "X": function(a, b) {return a * b},
-    "/": function(a, b) {return a / b},
-    //"%": ;
-    //"=": ;
-    //"AC": ;
-    //"CE": ;
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "X": (a, b) => a * b,
+    "/": (a, b) => a / b,
+    //"%": ,
+    //"=": ,
+    // next two don't work yet
+    "AC": (total, current, inputs) => {total = 0; current = "0"; inputs = []},
+    "CE": current => {current = "0";}
   };
   let inputs = [];
   
@@ -27,19 +28,20 @@ $(document).ready(function() {
           current = "";
         }
         current += value;
-        $("#answer").val(current.substr(0, 10));
+        $("#answer").val(current);
         
       // Clicked operator? --> store current and operator in inputs, execute calculate()
       } else if (value !== "AC" || value !== "CE") {
-        inputs.push(current);
+        inputs.push(parseFloat(current));
         inputs.push(value);
-        console.log(inputs);
         calculate();
-        current = "";
-      
+        $("#answer").val(total);
+        current = "0";
+        
       // Clicked erase button? --> erase last input or entire calculation
       } else {
         operators[value];
+        $("#answer").val(current.substr(0, 10));
       }
         
         
@@ -49,42 +51,14 @@ $(document).ready(function() {
     if (total === 0) {
       // If this is the first operator in the calculation, we only store the current number in *total*
       // This is necessary to show subtotals
-      total = inputs[0];
+      total = parseFloat(inputs[0]);
     } else {
       // If this is not the first operator, find the previous operator & input, and calculate the new total.
-      let tempOp = "";
-      console.log(inputs[-1]);
-      console.log(inputs[-2]);
-      console.log(inputs[-3]);
-      console.log(inputs[-4]);
-      // The next loop is necessary in order to make repeating the same operator (to repeat the last operation) work as expected
-      /*for (let i = 2; i < inputs.length - 1; i++) {
-        console.log(inputs[-i]);
-        if (isNaN(inputs[-i])) {
-          tempOp = inputs[-i];
-          console.log(inputs[-i]);
-          break;
-        }
-      }*/
-      
-      // Finally, execute the calculation!
-      operators[tempOp](total, current);
+      let tempOp = inputs[inputs.length-3];
+      total = operators[tempOp](total, parseFloat(current));
     }
-    $("#answer").val(total.substr(0, 10));
+    
   }
-  /*
-  function operate(myValue) {
-    if (currOp === "") {
-      currOp = myValue;
-    }
-    if (total === 0 && (currOp === "X" || currOp === "/")) {
-      total = 1;
-    }
-    total = operators[currOp](total, parseFloat(current));
-    current = "0";
-    $("#answer").val(total);
-  }
-  */
-  
+
 });
 

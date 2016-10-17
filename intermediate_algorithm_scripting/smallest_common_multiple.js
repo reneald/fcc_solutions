e6ccd626823b253e0d199b6927d0833631d413ed
Leander Van Reeth <delea.vanreeth@gmail.com>
2016-10-17 12:57:17 +0000

    some new solved algorithms

--- intermediate_algorithm_scripting/smallest_common_multiple.js
+++ intermediate_algorithm_scripting/smallest_common_multiple.js
@@ -1,35 +1,30 @@
 function smallestCommons(arr) {
-  arr = arr.sort();
-  
-  // Adding numbers in between the two given numbers, then sorting the array again
-  for (let i = arr[0] + 1; i < arr[1]; i++) {
+  "use strict";
+  arr = arr.sort((a, b) => a-b);
+  for(let i = arr[0] + 1; i < arr[1]; i++){
     arr.push(i);
   }
-  arr = arr.sort();
+  arr.sort((a, b) => a-b);
+  //return arr;  
   
-  /* Defining number to divide
-   * Start with largest number in array
-   * Continue with multiples of that number until smallest common multiple is found
-   */
-  let currNum = arr[-1];
-  
-  // Divide currNum by all numbers in arr
-  let i = 2;
-  while (!mult) {
-    for (let j = arr[-1]; j <= 0; j--) {
-      if (currNum % arr[j] !== 0) {
-        // Stop for loop if currNum can't be divided by arr[j]
-        break;
+  function testMultiple(a, b) {
+    let numDiv = [a, b].sort((a, b) => a-b);
+    let number = numDiv[1];
+    let divider = numDiv[0];
+    let i=1;
+    let mult=false;
+    while(mult===false){
+      if(number%divider!==0){
+        i++;
+        number=numDiv[1]*i;
+      } else{
+        mult=true;
+        return number;
       }
-      // If currNum can be divided by all numbers in array, we found the smallest common multiple
-      var mult = currNum;
     }
-    // If the loop stopped early, continue with the next multiple of arr[-1]
-    currNum *= i;
-    i++;
   }
-  
-  return mult;
+  return arr.reduce(testMultiple);  
+
 }
 
 
